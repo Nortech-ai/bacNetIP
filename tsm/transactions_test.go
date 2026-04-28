3b05dbf52abb66950bb4025b42f984335557e93a
Svein Roar Kvåle <162693718+SveinRoarKvale@users.noreply.github.com>
2026-04-28 09:12:41 +0200

    Revert "test(tsm): stabilize TestDataTransaction and modernize concurrency"
    This reverts commit 24e9119513a340ad71eba85eb47cb3583ee1fa8f.

--- tsm/transactions_test.go
+++ tsm/transactions_test.go
@@ -2,8 +2,6 @@ package tsm
 
 import (
 	"context"
-	"fmt"
-	"sync"
 	"testing"
 	"time"
 )
@@ -11,9 +9,9 @@ import (
 func TestTSM(t *testing.T) {
 	size := 3
 	tsm := New(size)
-	ctx := t.Context()
+	ctx := context.Background()
 	var err error
-	for range size - 1 {
+	for i := 0; i < size-1; i++ {
 		_, err = tsm.ID(ctx)
 		if err != nil {
 			t.Fatal(err)
@@ -40,7 +38,7 @@ func TestTSM(t *testing.T) {
 	}
 
 	// Now we should be able to get a new id since we free id
-	_, err = tsm.ID(t.Context())
+	_, err = tsm.ID(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -51,73 +49,51 @@ func TestDataTransaction(t *testing.T) {
 	size := 2
 	tsm := New(size)
 	ids := make([]int, size)
+	var err error
 
-	for i := range size {
-		id, err := tsm.ID(t.Context())
+	for i := 0; i < size; i++ {
+		ids[i], err = tsm.ID(context.Background())
 		if err != nil {
 			t.Fatal(err)
 		}
-		ids[i] = id
 	}
 
-	expected := map[int]string{
-		ids[0]: "Hello First ID",
-		ids[1]: "Hello Second ID",
-	}
-
-	var wg sync.WaitGroup
-	errCh := make(chan error, len(ids)*2)
-	recvCh := make(chan string, len(ids))
-
-	for _, id := range ids {
-		msg := expected[id]
-
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			if sendErr := tsm.Send(id, msg); sendErr != nil {
-				errCh <- fmt.Errorf("send %d: %w", id, sendErr)
-			}
-		}()
-
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			b, recvErr := tsm.Receive(id, 5*time.Second)
-			if recvErr != nil {
-				errCh <- fmt.Errorf("receive %d: %w", id, recvErr)
-				return
-			}
-
-			s, ok := b.(string)
-			if !ok {
-				errCh <- fmt.Errorf("type was not preserved for id %d", id)
-				return
-			}
-			recvCh <- s
-		}()
-	}
-
-	wg.Wait()
-	close(errCh)
-	close(recvCh)
-
-	for err := range errCh {
-		t.Fatal(err)
-	}
-
-	received := make(map[string]int, len(ids))
-	for msg := range recvCh {
-		received[msg]++
-	}
-
-	for _, msg := range expected {
-		if received[msg] != 1 {
-			t.Fatalf("expected message %q once, got %d", msg, received[msg])
+	go func() {
+		err = tsm.Send(ids[0], "Hello First ID")
+		if err != nil {
+			t.Error(err)
 		}
+	}()
+
+	go func() {
+		err = tsm.Send(ids[1], "Hello Second ID")
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	go func() {
+		b, err := tsm.Receive(ids[0], time.Duration(5)*time.Second)
+		if err != nil {
+			t.Error(err)
+		}
+		s, ok := b.(string)
+		if !ok {
+			t.Errorf("type was not preseved")
+			return
+		}
+		t.Log(s)
+	}()
+
+	b, err := tsm.Receive(ids[1], time.Duration(5)*time.Second)
+	if err != nil {
+		t.Error(err)
 	}
 
-	if len(received) != len(expected) {
-		t.Fatalf("expected %d unique messages, got %d", len(expected), len(received))
+	s, ok := b.(string)
+	if !ok {
+		t.Errorf("type was not preseved")
+		return
 	}
+	t.Log(s)
 }
